f6c498bafd55a7b84adaffb740a18d618aa984d4
[ivankabestwill] <[278467214@qq.com]>
2018-11-02 12:00:15 +0800

    add more

---
+++ tools/pre_build_nta.sh
@@ -0,0 +1,6 @@
+yum -y install epel-release && yum clean all
+yum -y install dh-autoreconf.noarch  gcc libpcap-devel pcre-devel libyaml-devel file-devel \
+zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel tar make \
+libnetfilter_queue-devel lua-devel cmake cmake3 jemalloc-devel ragel-devel \
+gcc-c++ bzip2-devel libzstd-devel lz4-devel libzip-devel snappy-devel \
+which perl openssl-devel libcurl-devel && yum clean all
